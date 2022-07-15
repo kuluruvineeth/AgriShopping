d853a1c698dca308712c6d3f69dba6eb60b745bf
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-15 07:14:18 +0530

    added custon button and edittext widgets

---
+++ app/src/main/java/com/kuluruvineeth/agrishopping/utils/MSPButton.kt
@@ -0,0 +1,23 @@
+package com.kuluruvineeth.agrishopping.utils
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatButton
+
+class MSPButton(context: Context, attrs: AttributeSet) : AppCompatButton(context,attrs) {
+
+    init{
+        //Call the function to apply the font to the components.
+        applyFont()
+    }
+
+    private fun applyFont(){
+        //This is used to get the file from the assets folder and set it to the button text.
+        val typeface : Typeface =
+            Typeface.createFromAsset(context.assets,"Montserrat-Bold.ttf")
+        setTypeface(typeface)
+    }
+
+
+}
