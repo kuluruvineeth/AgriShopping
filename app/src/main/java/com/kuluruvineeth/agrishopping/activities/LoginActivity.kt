67ddee0347901317d7d457fdf2b8b956801d0d16
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-16 07:18:10 +0530

    added intents for login and register activities.

--- app/src/main/java/com/kuluruvineeth/agrishopping/activities/LoginActivity.kt
+++ app/src/main/java/com/kuluruvineeth/agrishopping/activities/LoginActivity.kt
@@ -1,17 +1,19 @@
 package com.kuluruvineeth.agrishopping.activities
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.TextView
 import com.kuluruvineeth.agrishopping.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        val tv_register = findViewById<TextView>(R.id.tv_register)
         //Hide status bar
         @Suppress("DEPRECATION")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
@@ -21,6 +23,12 @@ class LoginActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
+        }
+
+        tv_register.setOnClickListener {
+            //Launch the register screen when the user clicks on the text.
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/kuluruvineeth/agrishopping/activities/RegisterActivity.kt
+++ app/src/main/java/com/kuluruvineeth/agrishopping/activities/RegisterActivity.kt
@@ -1,17 +1,19 @@
 package com.kuluruvineeth.agrishopping.activities
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.TextView
 import com.kuluruvineeth.agrishopping.R
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-
+        val tv_login = findViewById<TextView>(R.id.tv_login)
         //Hide status bar
         @Suppress("DEPRECATION")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
@@ -21,6 +23,12 @@ class RegisterActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
+        }
+
+        tv_login.setOnClickListener {
+            //Launch Logic screen on click
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 
