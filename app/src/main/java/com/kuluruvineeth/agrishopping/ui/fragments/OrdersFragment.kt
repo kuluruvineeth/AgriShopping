8de8e4a6f238d8ff4b0eef0200e318cddd65f086
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-17 17:25:22 +0530

    modified three fragments.

--- app/src/main/java/com/kuluruvineeth/agrishopping/ui/fragments/OrdersFragment.kt
+++ app/src/main/java/com/kuluruvineeth/agrishopping/ui/fragments/OrdersFragment.kt
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-//import com.kuluruvineeth.agrishopping.activities.databinding.FragmentNotificationsBinding
-import com.kuluruvineeth.agrishopping.databinding.FragmentNotificationsBinding
+//import com.kuluruvineeth.agrishopping.activities.databinding.FragmentOrdersBinding
+import com.kuluruvineeth.agrishopping.databinding.FragmentOrdersBinding
 
-class NotificationsFragment : Fragment() {
+class OrdersFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentOrdersBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class NotificationsFragment : Fragment() {
     ): View {
         //val notificationsViewModel = ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentOrdersBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
--- app/src/main/java/com/kuluruvineeth/agrishopping/ui/fragments/ProductsFragment.kt
+++ app/src/main/java/com/kuluruvineeth/agrishopping/ui/fragments/ProductsFragment.kt
@@ -8,11 +8,11 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 //import com.kuluruvineeth.agrishopping.activities.databinding.FragmentHomeBinding
-import com.kuluruvineeth.agrishopping.databinding.FragmentHomeBinding
+import com.kuluruvineeth.agrishopping.databinding.FragmentProductsBinding
 
-class HomeFragment : Fragment() {
+class ProductsFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentProductsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class HomeFragment : Fragment() {
     ): View {
         //val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentProductsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
