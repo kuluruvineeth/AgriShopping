6b69525608537867735749edc66f815a69d3cdf4
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-18 09:25:26 +0530

    added product activity and menu.

--- app/src/main/java/com/kuluruvineeth/agrishopping/ui/fragments/ProductsFragment.kt
+++ app/src/main/java/com/kuluruvineeth/agrishopping/ui/fragments/ProductsFragment.kt
@@ -1,14 +1,16 @@
 package com.kuluruvineeth.agrishopping.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.kuluruvineeth.agrishopping.R
 //import com.kuluruvineeth.agrishopping.activities.databinding.FragmentHomeBinding
 import com.kuluruvineeth.agrishopping.databinding.FragmentProductsBinding
+import com.kuluruvineeth.agrishopping.ui.activities.AddProductActivity
+import com.kuluruvineeth.agrishopping.ui.activities.SettingsActivity
 
 class ProductsFragment : Fragment() {
 
@@ -17,6 +19,12 @@ class ProductsFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //If we want to use the option menu in fragment we need to add it
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,5 +47,21 @@ class ProductsFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_product_menu,menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        when(id){
+            R.id.action_add_product -> {
+                startActivity(Intent(activity, AddProductActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
