8d08a3ee787758f835e84aee9cbf6653fd0c22cb
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-18 09:49:28 +0530

    designed add product activity screen.

--- app/src/main/java/com/kuluruvineeth/agrishopping/ui/activities/AddProductActivity.kt
+++ app/src/main/java/com/kuluruvineeth/agrishopping/ui/activities/AddProductActivity.kt
@@ -2,11 +2,24 @@ package com.kuluruvineeth.agrishopping.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.kuluruvineeth.agrishopping.R
+import kotlinx.android.synthetic.main.activity_add_product.*
 
 class AddProductActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_product)
+        setupActionBar()
+    }
+    private fun setupActionBar(){
+        setSupportActionBar(toolbar_add_product_activity)
+
+        val actionBar = supportActionBar
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+        }
+        toolbar_add_product_activity.setNavigationOnClickListener { onBackPressed() }
     }
 }
