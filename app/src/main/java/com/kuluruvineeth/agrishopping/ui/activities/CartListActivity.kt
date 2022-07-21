8945d9b54584ec1761a172f1413e937e820a7c39
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-21 07:24:55 +0530

    completed cart item list layout design.

--- app/src/main/java/com/kuluruvineeth/agrishopping/ui/activities/CartListActivity.kt
+++ app/src/main/java/com/kuluruvineeth/agrishopping/ui/activities/CartListActivity.kt
@@ -2,9 +2,12 @@ package com.kuluruvineeth.agrishopping.ui.activities
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kuluruvineeth.agrishopping.R
 import com.kuluruvineeth.agrishopping.firestore.FirestoreClass
 import com.kuluruvineeth.agrishopping.models.CartItem
+import com.kuluruvineeth.agrishopping.ui.adapters.CartItemsListAdapter
 import kotlinx.android.synthetic.main.activity_cart_list.*
 
 class CartListActivity : BaseActivity() {
@@ -16,8 +19,37 @@ class CartListActivity : BaseActivity() {
 
     fun successCartItemsList(cartList: ArrayList<CartItem>){
         //hideProgressDialog()
-        for(i in cartList){
-            Log.i("Cart Item Title",i.title)
+        if(cartList.size > 0){
+            rv_cart_items_list.visibility = View.VISIBLE
+            ll_checkout.visibility = View.VISIBLE
+            tv_no_cart_item_found.visibility = View.GONE
+
+            rv_cart_items_list.layoutManager = LinearLayoutManager(this)
+            rv_cart_items_list.setHasFixedSize(true)
+            val cartListAdapter = CartItemsListAdapter(this,cartList)
+            rv_cart_items_list.adapter = cartListAdapter
+            var subTotal: Double = 0.0
+            for(item in cartList){
+                val price = item.price.toDouble()
+                val quantity = item.cart_quantity.toInt()
+                subTotal += (price * quantity)
+            }
+            tv_sub_total.text = "$subTotal"
+            tv_shipping_charge.text = "Rs.30.0" //TODO change shipping charge logic
+
+            if(subTotal > 0){
+                ll_checkout.visibility = View.VISIBLE
+
+                val total = subTotal + 10 //TODO - Change Logic here
+                tv_total_amount.text = "$total"
+            }else{
+                ll_checkout.visibility = View.GONE
+            }
+        }
+        else{
+            rv_cart_items_list.visibility = View.GONE
+            ll_checkout.visibility = View.GONE
+            tv_no_cart_item_found.visibility = View.VISIBLE
         }
     }
 
