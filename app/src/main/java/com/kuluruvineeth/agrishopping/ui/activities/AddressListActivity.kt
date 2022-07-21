4d800cab53dcafa69a263827e81393e940302013
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-21 10:08:21 +0530

    created address list activity.

---
+++ app/src/main/java/com/kuluruvineeth/agrishopping/ui/activities/AddressListActivity.kt
@@ -0,0 +1,24 @@
+package com.kuluruvineeth.agrishopping.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kuluruvineeth.agrishopping.R
+import kotlinx.android.synthetic.main.activity_address_list.*
+
+class AddressListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_address_list)
+        setupActionBar()
+    }
+    private fun setupActionBar(){
+        setSupportActionBar(toolbar_address_list_activity)
+
+        val actionBar = supportActionBar
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+        }
+        toolbar_address_list_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+}
