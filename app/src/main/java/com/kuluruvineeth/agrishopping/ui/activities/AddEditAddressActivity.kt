2af41bf30e4307af5a699446e5bd3d06ff520aeb
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-22 06:52:20 +0530

    created add edit address activity and UI.

--- app/src/main/java/com/kuluruvineeth/agrishopping/ui/activities/AddEditAddressActivity.kt
+++ app/src/main/java/com/kuluruvineeth/agrishopping/ui/activities/AddEditAddressActivity.kt
@@ -1,30 +1,25 @@
 package com.kuluruvineeth.agrishopping.ui.activities
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kuluruvineeth.agrishopping.R
-import kotlinx.android.synthetic.main.activity_address_list.*
+import kotlinx.android.synthetic.main.activity_add_edit_address.*
 
-class AddressListActivity : AppCompatActivity() {
+class AddEditAddressActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_address_list)
+        setContentView(R.layout.activity_add_edit_address)
         setupActionBar()
-
-        tv_add_address.setOnClickListener{
-            val intent = Intent(this,AddEditAddressActivity::class.java)
-            startActivity(intent)
-        }
     }
     private fun setupActionBar(){
-        setSupportActionBar(toolbar_address_list_activity)
+        setSupportActionBar(toolbar_add_edit_address_activity)
 
         val actionBar = supportActionBar
         if(actionBar != null){
             actionBar.setDisplayHomeAsUpEnabled(true)
             actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
         }
-        toolbar_address_list_activity.setNavigationOnClickListener { onBackPressed() }
+        toolbar_add_edit_address_activity.setNavigationOnClickListener { onBackPressed() }
+
     }
 }
