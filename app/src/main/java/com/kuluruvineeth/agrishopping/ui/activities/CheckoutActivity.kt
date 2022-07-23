0bc7279006702f313783e8bf68c1a12dcd3e3bdb
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-23 07:52:16 +0530

    displaying address on checkout screen.

--- app/src/main/java/com/kuluruvineeth/agrishopping/ui/activities/CheckoutActivity.kt
+++ app/src/main/java/com/kuluruvineeth/agrishopping/ui/activities/CheckoutActivity.kt
@@ -3,13 +3,33 @@ package com.kuluruvineeth.agrishopping.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kuluruvineeth.agrishopping.R
+import com.kuluruvineeth.agrishopping.models.Address
+import com.kuluruvineeth.agrishopping.utils.Constants
 import kotlinx.android.synthetic.main.activity_checkout.*
 
 class CheckoutActivity : AppCompatActivity() {
+
+    private var mAddressDetails: Address? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkout)
         setupActionBar()
+
+        if(intent.hasExtra(Constants.EXTRA_SELECTED_ADDRESS)){
+            mAddressDetails = intent.getParcelableExtra(Constants.EXTRA_SELECTED_ADDRESS)
+        }
+
+        if(mAddressDetails != null){
+            tv_checkout_address_type.text = mAddressDetails?.type
+            tv_checkout_full_name.text = mAddressDetails?.name
+            tv_checkout_address.text = "${mAddressDetails!!.address}, ${mAddressDetails!!.zipCode}"
+            tv_checkout_additional_note.text = mAddressDetails?.additionalNote
+
+            if(mAddressDetails?.otherDetails!!.isNotEmpty()){
+                tv_checkout_other_details.text = mAddressDetails?.otherDetails
+            }
+            tv_mobile_number.text = mAddressDetails?.mobileNumber
+        }
     }
     private fun setupActionBar(){
         setSupportActionBar(toolbar_checkout_activity)
