ee6ce90ab275964648eb205a520c4ba189fe4aef
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-23 12:20:04 +0530

    prepared sold products fragment and edited navigation bar.

--- app/src/main/java/com/kuluruvineeth/agrishopping/ui/activities/DashboardActivity2.kt
+++ app/src/main/java/com/kuluruvineeth/agrishopping/ui/activities/DashboardActivity2.kt
@@ -34,7 +34,10 @@ class DashboardActivity2 : BaseActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_dashboard,R.id.navigation_products, R.id.navigation_orders
+                R.id.navigation_dashboard,
+                R.id.navigation_products,
+                R.id.navigation_orders,
+                R.id.navigation_sold_products
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
